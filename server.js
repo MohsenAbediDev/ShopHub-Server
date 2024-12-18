081dbc23a3ee8b7ed3f1a6150187091f2ab9b649
Mohsen <mohsen63296329@gmail.com>
2024-12-18 22:20:31 +0330

    fix products route

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
  
 server.use(middlewares)
-server.use('/products', router)
+server.use('/', router)
+
 server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running')
 })
-
